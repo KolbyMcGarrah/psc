ef4a86d6be7bdea8cb91e6084d2a323098ed093f
KolbyMcGarrah <Kolbymcgarrah@gmail.com>
2019-08-20 12:24:18 -0600

    Update settings.py

--- psc/settings.py
+++ psc/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = 'os.path.join(BASE_DIR, 'staticfiles')'
-
+STATIC_ROOT = ''
+#os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join('static'),)
@@ -180,4 +180,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #EMAIL_PORT = 587
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
